e5acd55ab127b3e7b8fa1352817b914c2d876ae9
Aubrey Collins <aubreypc@users.noreply.github.com>
2019-04-22 21:51:34 -0400

    Parsing test

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -13,7 +13,21 @@ def test_remove_comment():
     assert parser._remove_comment(f"{uncommented} #{comment}") == uncommented
 
 
-@pytest.mark.skip(reason="dev")
+def test_match_end_of_block():
+    parser = GraphQLParser(None)
+    assert parser._match_end_of_block("}")
+    assert not parser._match_end_of_block("")
+    assert not parser._match_end_of_block("attr")
+
+
+def test_match_start_of_block():
+    parser = GraphQLParser(None)
+    assert parser._match_start_of_block("{")
+    assert parser._match_start_of_block("obj {")
+    assert parser._match_start_of_block("query obj(arg1: $arg1) {")
+    assert not parser._match_start_of_block("")
+
+
 def test_parse_block():
     name = Field("name", fieldtype=str, nullable=False)
     age = Field("age", fieldtype=int, nullable=False)
@@ -21,14 +35,24 @@ def test_parse_block():
     ctx = GraphQLContext([person])
     parser = GraphQLParser(ctx)
 
-    blocks = parser.parse("""
+    blocks = parser.parse(
+        """
     {
         person(id: 1){
             name
             age
         }
+
+        person(id: 2) {
+            age
+        }
     }
-    """)
+    """
+    )
     blocks = [block for block in blocks]
+    print([(block, block.blocktype.name, block.attrs) for block in blocks])
     assert isinstance(blocks[0], GraphQLBlock)
     assert blocks[0].blocktype.name == "person"
+    assert blocks[0].attrs[0] == "name"
+    assert blocks[0].attrs[1] == "age"
+    assert blocks[1].attrs[0] == "age"
