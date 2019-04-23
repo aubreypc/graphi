b37a77ffaa23f5e4cbdbfb3e380f6166b3292f85
Aubrey Collins <aubreypc@users.noreply.github.com>
2019-04-23 17:17:11 -0400

    Add method to get SQL table creation command for GraphQL type

--- graphi/query.py
+++ graphi/query.py
@@ -9,11 +9,6 @@ class GraphQLQuery:
         pass
 
 
-class GraphQLContext:
-    def __init__(self, types: List[GraphQLType]):
-        self.types = types
-
-
 class GraphQLBlock:
     def __init__(
         self,
@@ -28,6 +23,29 @@ class GraphQLBlock:
         self.operation = operation
 
 
-class GraphQLExpression:
-    def __init__(self):
+class GraphQLContext:
+    def __init__(self, types: List[GraphQLType]):
+        self.types = {t.name: t for t in types}
+
+    def create_tables(self):
+        """ Performs SQL table setup for each defined GraphQL type """
+        map_sql_types = {str: "TEXT", int: "INTEGER"}
+        statements = []
+        for t in self.types.values():
+            fields = ["id INTEGER PRIMARY KEY"]
+            for field in t.fields:
+                if field.fieldtype in map_sql_types:
+                    fields.append(f"{field.name} {map_sql_types[field.fieldtype]}")
+                    # TODO: nullable?
+                else:
+                    # TODO: foreign key
+                    pass
+            fields_string = ",\n".join(fields)
+            statements.append(
+                f"CREATE TABLE IF NOT EXISTS {t.name} (\n{fields_string}\n);"
+            )
+        return "\n".join(statements)
+
+    def resolve(self, block: GraphQLBlock):
+        """ Attempts to execute a query or mutation """
         pass
