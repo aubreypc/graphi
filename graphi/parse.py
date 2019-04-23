35e9054bbbc694fb8ad7e1f0b2f49fd6c7480023
Aubrey Collins <aubreypc@users.noreply.github.com>
2019-04-22 21:50:43 -0400

    Parsing logic

--- graphi/parse.py
+++ graphi/parse.py
@@ -9,55 +9,56 @@ from typing import List, Iterator
 class GraphQLParser:
     def __init__(self, ctx: GraphQLContext):
         self.context = ctx
-    
-    def parse(self, s: str) -> Iterator[GraphQLBlock]:
-        """ Parses a GraphQL expression string, yielding GraphQLBlock objects """
-        query_stack = []
-        for j, char in enumerate(s):
-            if char == "{":
-                query_stack.append(j)
-            elif char == "}":
-                i = query_stack.pop()
-                substr = s[i + 1 : j]
-                lines = substr.split("\n")
-                yield from self._parse_block_lines(lines)
 
-    def _parse_block_lines(self, lines: List[str]) -> Iterator[GraphQLBlock]:
+    def parse(self, s: str) -> Iterator[GraphQLBlock]:
         blocks = []
-        attrs = []
-        for line in lines:
+        for line in s.split("\n"):
             line = self._remove_comment(line)
+            if not line:
+                continue
 
             # RegEx matching
             match_start_of_block = self._match_start_of_block(line)
             match_end_of_block = self._match_end_of_block(line)
-            match_block_attr = self._block_attr(line)
+            match_block_attr = self._match_block_attr(line)
+            match_fragment = self._match_fragment(line)
 
             if match_start_of_block:
-                block_name, block_args = match_start_of_block.groups()
-                new_block = GraphQLBlock()
+                operation, block_name, block_args = match_start_of_block.groups()
+                inferred_blocktype = GraphQLType(name=block_name)
+                new_block = GraphQLBlock(
+                    blocktype=inferred_blocktype, operation=operation
+                )
                 blocks.append(new_block)
             elif match_end_of_block:
                 next_block = blocks.pop()
-                next_block.attrs = attrs
+                if blocks:
+                    blocks[-1].attrs.append(next_block)
                 yield next_block
-                attrs = [next_block]
-            elif line:
-                attrs.append(line.strip())  # TODO: should do a regex for single word
-            # TODO: write regex to detect:
-            # - arguments
-            # - nested query on object
-            # - fragments
+            elif match_block_attr:
+                if blocks:
+                    print(f"Adding line {line} to block {blocks[-1]}")
+                blocks[-1].attrs.append(line)  # TODO: should do a regex for single word
+            elif match_fragment:
+                # TODO: handle fragments
+                pass
+            else:
+                raise Exception(f"Could not parse {line}")
 
     def _match_start_of_block(self, line: str):
         """ Use RegEx to match the start of GraphQL block """
-        return re.match(r"^(?:query\s)?(\w+)?(?(1)(\(.+\))?|)\s?{", line.strip())
-    
+        return re.match(
+            r"^(query\s|mutation\s)?(\w+)?(?(2)(\(.+\))?|)\s?{", line.strip()
+        )
+
     def _match_end_of_block(self, line: str):
         return line.endswith("}")
-    
+
     def _match_block_attr(self, line: str):
-        return line
+        return re.match(r"^\b\w+\b$", line)
+
+    def _match_fragment(self, line: str):
+        return re.match(r"^...\b\w+\b$", line)
 
     def _remove_comment(self, line: str) -> str:
         if "#" in line:
